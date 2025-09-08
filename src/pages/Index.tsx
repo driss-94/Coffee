e6e957fd2607b9e277dca593969e0c324b991fed
driss-94 <elandlosydriss@gmail.com>
2025-09-08 00:58:00 -0500

    Initial commit for Coffee project

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+const Index = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-gray-900">Universal Coffee Shop App</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
